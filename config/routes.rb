f85d87a5ecb1ae1fbf2dc0276f7e35fa0b7057e4
rsoule3 <robsoule3@gmail.com>
2014-10-26 10:47:02 -0400

    Setting up Guard, cleaning

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
-  get 'static_pages/home'
+  root 'static_pages#home'
   get 'static_pages/contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
