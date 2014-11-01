12052eb89313a17b67d233ca41f3879d81cc0ba7
rsoule3 <robsoule3@gmail.com>
2014-11-01 16:30:58 -0400

    fixing post errors

--- app/assets/javascripts/components/post.js.jsx
+++ app/assets/javascripts/components/post.js.jsx
@@ -15,7 +15,7 @@ var PostHeader = React.createClass({
   render: function() {
     return (
       <div className="post-header">
-        <h2>{this.props.post.title}</h2>
+        <h1>{this.props.post.title}</h1>
         <div className="post-meta">
           By {this.props.post.author} - {this.props.post.created_at}
         </div>
